73ba8262b0cdf663424d82cb5d7572b6d6490583
PivotalSaint <licensedmt@att.net>
2022-09-13 15:54:00 -0500

    module 9.1 full code

--- app.js
+++ app.js
@@ -2,14 +2,51 @@ console.log('Hello World!');
 
 var message = 'Hello Node!';
 
-var sum = 60 + 9 + ('420');
+// old school way
+// var sum = 60 + 9 + (commandLineArgs);
 
 var commandLineArgs = process.argv;
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 console.log(commandLineArgs);
 console.log(message);
 console.log(sum);
+// notice the lack of parentheses around the `profileDataArr` parameter?
+
+// const printProfileData = profileDataArr => {
+    // for (let i < 0)
+    // console.log(profileDataArr);
+// };
+
+// printProfileData(profileDataArgs);
+
+// Using Function expression syntax
+// const addNums = function(numOne, numTwo) {
+    // return numOne + numTwo;
+// };
+
+// Using new arrow function syntax
+// const addNums = (numOne, numTwo) => {
+    // return numOne + numTwo;
+// };
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
+
+// node app.js function.function 
+
 
 // For challenge
 // node index.js will be used to call your project
