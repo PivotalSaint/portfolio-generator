e37f10c5bc5426981e0fffec02a7faf68cd1ebe8
PivotalSaint <licensedmt@att.net>
2022-09-13 21:04:37 -0500

    generate the HTML File

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs= require('fs');
+const generatePage = require('./src/page-template.js');
 // console.log('Hello World!');
 
 // var message = 'Hello Node!';
@@ -72,25 +73,9 @@ const [name, github] = profileDataArgs;
  // `;
 //};
 
+// we cut this generate page function and moved it to page-template.js
 //updated generatePage function 9.2.4
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
 
 
 // added this console log in 9.2.4 
